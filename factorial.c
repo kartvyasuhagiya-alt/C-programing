142319cfba0c501179e2a55bd73fc8ddd31c78b0
kartvyasuhagiya-alt <kartvyasuhagiya@gmail.com>
2025-09-12 10:21:43 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int factorial(int n )
+{
+   int ans = 1, i;
+   for (i = 2; i<=n; i++)
+   ans *= i;
+   return ans ;
+   }
+   int main()
+   {
+      int num =4;
+      printf("%d\n",factorial(num));
+      return 0;
+      }
+
+
+
+
+
+
