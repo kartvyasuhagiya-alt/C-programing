3bf710fa40bd9fe9d5cd6215d4fb99c919184e42
kartvyasuhagiya-alt <kartvyasuhagiya@gmail.com>
2025-09-19 10:06:34 +0530

    Add files via upload

---
+++ areaperimeterofrectangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+      int length, breadth;
+
+      printf("enter the value of length:");
+      scanf("%d" , &length);
+      printf("enter the value of breadth:");
+      scanf("%d" , &breadth);
+      printf("area = %d\n", length * breadth);
+      printf("perimeter = %d\n", 2*(length+breadth));
+      return 0;
+      }
+
+
+
+
+
---
+++ areaperimeterofsquare.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+      int side, area, perimeter;
+      printf("enter the value of length:");
+      scanf("%d" , &side);
+      area = side * side;
+      perimeter = 4*side;
+      printf("%d\n", area);
+      printf("%d\n", perimeter);
+      return 0;
+      }
+
+
+
+
+
+
+
+
+
+
+
